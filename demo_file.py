5324e484ebb537ab2c5a8ee551fbc987e2fa6f58
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-01-15 13:44:19 +0530

    Add files via upload

---
+++ demo_file.py
@@ -0,0 +1,26 @@
+x = 52
+y = 14
+'''
+function - sum(),max(),min(),len(),type()
+import keyword
+
+print(keyword.kwlist)'''
+
+#Print statement
+print("This is python..")
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.""")
+
+print(x,end = " ")
+print("hello")
+print(x,y,"Values",sep = ">")
+
+
+
