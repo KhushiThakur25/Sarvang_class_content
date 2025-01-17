10bd45c92936f344c1c250d96cb4c921327fdd11
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-01-17 15:09:36 +0530

    Add files via upload

---
+++ Input_class.py
@@ -0,0 +1,25 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> input()
+5
+'5'
+>>> x = input("Enter the value")
+Enter the value12
+>>> y = input("Enter the second value")
+Enter the second value3
+>>> x + y
+'123'
+>>> "hello" + "world"
+'helloworld'
+>>> x = int(input("Enter the number"))
+Enter the number12
+>>> y = int(input("Enter the second number"))
+Enter the second number3
+>>> x + y
+15
+>>> x = float(input("Enter the number"))
+Enter the number12
+y = float(input("Enter the second number"))
+Enter the second number3
+x +y
+15.0
